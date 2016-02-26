7e420ddbac1b4bd6f22ccbafc52f71734e729c09
sz <szhangut@gmail.com>
2016-02-25 21:43:19 -0800

    add file

---
+++ test1.h
@@ -0,0 +1,13 @@
+//
+//  test1.h
+//  opengl_tut
+//
+//  Created by Sheng Zhang on 2/25/16.
+//  Copyright © 2016 Sheng Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test1 : NSObject
+
+@end
