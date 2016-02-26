2588e9746cb6d4d4d72d75bda8543deebb285102
SZ <szhangut@gmail.com>
2016-02-25 20:33:52 -0800

    first commit

---
+++ opengl_tut_1/tutorial2.hpp
@@ -0,0 +1,14 @@
+//
+//  tutorial2.hpp
+//  opengl_tut_1
+//
+//  Created by Sheng Zhang on 2/24/16.
+//  Copyright © 2016 Sheng Zhang. All rights reserved.
+//
+
+#ifndef tutorial2_hpp
+#define tutorial2_hpp
+
+#include <stdio.h>
+
+#endif /* tutorial2_hpp */
