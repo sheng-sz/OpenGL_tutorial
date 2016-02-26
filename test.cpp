ad96afcecfcf62f72ea90e19fea1af1e4b84cf13
SZ <szhangut@gmail.com>
2016-02-25 20:53:45 -0800

    test

---
+++ test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  opengl_tut
+//
+//  Created by Sheng Zhang on 2/25/16.
+//  Copyright © 2016 Sheng Zhang. All rights reserved.
+//
+
+#include "test.hpp"
---
+++ test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  opengl_tut
+//
+//  Created by Sheng Zhang on 2/25/16.
+//  Copyright © 2016 Sheng Zhang. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
