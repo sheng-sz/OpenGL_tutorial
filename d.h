f638cad184d842647c83be1fadd34353c369e39b
SZ <sz@gmail.com>
2016-02-25 22:27:54 -0800

    x

---
+++ d.h
@@ -0,0 +1,13 @@
+//
+//  d.h
+//  opengl_tut
+//
+//  Created by Sheng Zhang on 2/25/16.
+//  Copyright © 2016 Sheng Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface d : NSObject
+
+@end
---
+++ d.m
@@ -0,0 +1,13 @@
+//
+//  d.m
+//  opengl_tut
+//
+//  Created by Sheng Zhang on 2/25/16.
+//  Copyright © 2016 Sheng Zhang. All rights reserved.
+//
+
+#import "d.h"
+
+@implementation d
+
+@end
